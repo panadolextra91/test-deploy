75b38cdcbb481ca1d7ab49c0c5f610648747f338
panadolextra91 <126870223+panadolextra91@users.noreply.github.com>
2025-04-01 05:41:23 +0700

    config.js in fe

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE_URL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api" || "https://medimaster-be.vercel.app/api"
+    API_BASE_URL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api" || "https://medimaster-be.vercel.app"
 };
 
 export default config;
