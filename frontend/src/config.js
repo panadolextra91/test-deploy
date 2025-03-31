b98eee9c5e79e9b8106ec1cca89d7ff35816dd95
panadolextra91 <126870223+panadolextra91@users.noreply.github.com>
2025-04-01 05:24:31 +0700

    add base url of vercel be into fe

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const config = {
-    API_BASE_URL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api"  // Adjust the URL and port as needed
+    API_BASE_URL: process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api" || "https://medimaster-be.vercel.app/api"
 };
 
 export default config;
